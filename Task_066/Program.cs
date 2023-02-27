12a2b3ee877c4dc46b39331f825323cff8d8765e
Annita Maniri <annitk@gmail.com>
2023-02-27 14:32:50 +0200

    Task_066

---
+++ Task_066/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Enter M: ");
+int startNum = int.Parse(ReadLine());
+Write("Enter N: ");
+int endNum = int.Parse(ReadLine());
+
+WriteLine($"Sum of natural elements between M and N is {SumNaturNums(startNum, endNum)}");
+
+int SumNaturNums(int localStartNum, int localEndNum)
+{
+    int intSum;
+    if (localEndNum == localStartNum) return localEndNum;
+    else intSum = localEndNum + SumNaturNums(localStartNum, localEndNum - 1);
+    return intSum;
+}
+
